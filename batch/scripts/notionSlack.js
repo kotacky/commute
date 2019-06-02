415def3fc4c8843625cb812a1fcef492fb1238b3
yuu3an <39237387+yuu3an@users.noreply.github.com>
2019-06-02 17:14:08 +0900

    notionSlack.jsの不具合対応
    ファイルをutf-8で読み込む、全体的なリファクタ

--- batch/scripts/notionSlack.js
+++ batch/scripts/notionSlack.js
@@ -1,45 +1,68 @@
-// node_modulesのrequest-promise
 const request = require('request-promise');
 const _ = require('lodash');
-var fs =  require('fs');
+const fs =  require('fs');
+
+// 定期券の期限が切れるユーザのメールアドレスのリスト。実際はDBから取得したファイルを読みこんだものを使う
 const emailAddressJson = require('../output/employee.json');
-// 使用するtokenを設定
-const userToken = "";
-let usersList;
-// 定期券の期限が切れるユーザのメールアドレスのリスト。実際はDBから取得したファイルを読みこんだものを使う。
+const userToken = fs.readFileSync('../token.txt', 'utf-8');
 const expiredEmailAddressList = emailAddressJson;
-request({
-    url: 'https://slack.com/api/users.list',
-    method: 'post',
-    form: {
-        token: userToken
+
+// ユーザーリストを取得するための設定
+const users_list = function(userToken) {
+  let ret = {
+    uri: 'https://slack.com/api/users.list',
+    method: 'GET',
+    timeout: 30 * 1000,
+    qs: {
+      // なぜか改行コードが入るため削除する
+      token: userToken.replace(/\r?\n/g, '')
     }
-}, function (error, response, body) {
-    // 実際に使う内容はbody。JSON形式にparseする。
-    let jsonbody = JSON.parse(body);
-    // 'members'の中身を使う
-    usersList = jsonbody['members'];
-})
-    // requestの結果を受けて処理をする
-    .then(function () {
-    // 期限が切れたメールアドレスそれぞれについて、「expired」という変数を使って中身をループする
-    expiredEmailAddressList.forEach(function (expired) {
-        // lodashのfindメソッドで、APIを使用して取得したユーザのリストについて、メールアドレスがexpiredとマッチするユーザデータを探す
-        let user = _.find(usersList, function (user) {
-            return _.includes(user['profile'].email, expired['mailAddress']);
-        });
-        // 通知用に、ユーザ名に@をつける
-        user = '@' + user['name'];
-        request({
-            url: 'https://slack.com/api/chat.postMessage',
-            method: 'POST',
-            form: {
-                token: userToken,
-                channel: user,
-                text: 'テスト'
-            }
-        }, function (error, response, body) {
-          //  console.log(response);
-        });
+  }
+  return ret;
+};
+// 対象ユーザーに通知するための設定
+const chat_postMessage = function(userToken, user) {
+  let ret = {
+    url: 'https://slack.com/api/chat.postMessage',
+    timeout: 30 * 1000,
+    method: 'POST',
+    form: {
+      // なぜか改行コードが入るため削除する
+      token: userToken.replace(/\r?\n/g, ''),
+      channel: user,
+      text: 'テスト'
+    }
+  };
+  return ret;
+};
+
+request(users_list(userToken)).then(function(body) {
+  console.log('【success】users.list');
+
+  let jsonbody = JSON.parse(body);
+  let usersList = jsonbody['members'];
+
+  expiredEmailAddressList.forEach(function (expired) {
+    // 期限切れのユーザを探す
+    let user = _.find(usersList, function (user) {
+      return _.includes(user['profile'].email, expired['mailAddress']);
     });
+
+    // 通知用に、ユーザ名に@をつける
+    user = '@' + user['name'];
+
+    request(chat_postMessage(userToken, user)).then(function(body) {
+      console.log('【success】chat.postMessage');
+      console.log(body);
+    }).catch(function (err) {
+      console.log('【fail】chat.postMessage');
+      console.error(err);
+    });
+
+  });
+
+}).catch(function (err) {
+  console.log('【fail】users.list');
+  console.error(err);
 });
+
