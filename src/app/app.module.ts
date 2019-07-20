16111cbd3c96969a42ecafbbaa94f68907a56b3c
Yutaka0123 <y-takahashi@wiss1.co.jp>
2019-07-20 16:09:54 +0900

    easyからのパラメータ受け取りサンプル

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule, ActivatedRoute } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UsersComponent } from './components/users/users.component';
 import { HttpClientModule } from '@angular/common/http';
+
+const routes: Routes = [
+  {
+    path: 'test', component: UsersComponent
+  },
+];
 
 @NgModule({
   declarations: [
@@ -14,9 +21,24 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+  param1: string;
+  param2: string;
+  constructor(private route: ActivatedRoute) {
+    console.log('Called Constructor');
+    this.route.queryParams.subscribe(params => {
+        this.param1 = params['access_token'];
+        this.param2 = params['param2'];
+        console.log(this.param1)
+        if (this.param1) {
+        location.href='http://localhost:9001/test';
+        }
+    });
+  }
+}
